4801be68487ddcf017c0442253aafa435c375780
Isabella Bates <isba8475@colorado.edu>
2024-04-03 14:19:07 -0600

    welcome test case

---
+++ ProjectSourceCode/src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/welcome', (req, res) => {
+  res.json({status: 'success', message: 'Welcome!'});
+});
+
+module.exports = app.listen(3000);
