4df96a050a7c438edc3dee2e7bde424f7cada32b
Pandabear1125 <jvsdestroyer25@gmail.com>
2024-04-03 14:48:27 -0600

    bruh

---
+++ bruh/index.js
@@ -0,0 +1,179 @@
+// *****************************************************
+// <!-- Section 1 : Import Dependencies -->
+// *****************************************************
+
+const express = require('express'); // To build an application server or API
+const app = express();
+const handlebars = require('express-handlebars');
+const Handlebars = require('handlebars');
+const path = require('path');
+const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
+const bodyParser = require('body-parser');
+const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const bcrypt = require('bcrypt'); //  To hash passwords
+const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
+
+// *****************************************************
+// <!-- Section 2 : Connect to DB -->
+// *****************************************************
+
+// create `ExpressHandlebars` instance and configure the layouts and partials dir.
+const hbs = handlebars.create({
+    extname: 'hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+});
+
+// database configuration
+const dbConfig = {
+    host: 'db', // the database server
+    port: 5432, // the database port
+    database: process.env.POSTGRES_DB, // the database name
+    user: process.env.POSTGRES_USER, // the user account to connect with
+    password: process.env.POSTGRES_PASSWORD, // the password of the user account
+};
+
+const db = pgp(dbConfig);
+
+// test your database
+db.connect()
+    .then(obj => {
+        console.log('Database connection successful'); // you can view this message in the docker compose logs
+        obj.done(); // success, release the connection;
+    })
+    .catch(error => {
+        console.log('ERROR:', error.message || error);
+    });
+
+// *****************************************************
+// <!-- Section 3 : App Settings -->
+// *****************************************************
+
+// Register `hbs` as our view engine using its bound `engine()` function.
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
+
+// initialize session variables
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        saveUninitialized: false,
+        resave: false,
+    })
+);
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+// *****************************************************
+// <!-- Section 4 : API Routes -->
+// *****************************************************
+
+// TODO - Include your API routes here
+
+app.get('/', (req, res) => {
+    res.redirect('/login');
+});
+
+// Register
+app.get("/register", (req, res) => {
+    res.render("pages/register");
+});
+
+app.post('/register', async (req, res) => {
+    //hash the password using bcrypt library
+    const hash = await bcrypt.hash(req.body.password, 10);
+    console.log(hash);
+    // To-DO: Insert username and hashed password into the 'users' table
+    const insert_query = "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *;";
+    db.any(insert_query, [req.body.username, hash])
+        .then(function (data) {
+            res.redirect("/login");
+        })
+        .catch(function (err) {
+            console.log(err);
+            res.redirect("/register");
+        })
+});
+
+// Login
+app.get('/login', (req, res) => {
+    res.render("pages/login");
+});
+
+app.post('/login', async (req, res) => {
+
+    // get user from table
+    const find_user = "SELECT * FROM users WHERE username = $1;";
+
+    db.any(find_user, [req.body.username])
+        .then(async function (data) {
+            var user = data[0];
+            console.log(user);
+
+            // check if password from request matches with password in DB
+            const match = await bcrypt.compare(req.body.password, user.password);
+
+            if (!match) {
+                res.render("pages/login", { error: true, message: "1Incorrect username or password.", });
+            }
+            else {
+                // save the data in the session var
+                req.session.user = user;
+                req.session.save();
+
+                res.redirect("/discover");
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+            res.render("pages/register", { error: true, message: "User does not exist.", });
+        })
+});
+
+app.get("/discover", (req, res) => {
+    if (!req.session.user)
+    {
+        res.redirect("/login");
+        return;
+    }    
+        
+
+    axios({
+        url: `https://app.ticketmaster.com/discovery/v2/events.json`,
+        method: 'GET',
+        dataType: 'json',
+        headers: {
+            'Accept-Encoding': 'application/json',
+        },
+        params: {
+            apikey: process.env.API_KEY,
+            size: 10
+        },
+    }) 
+        .then(results => {
+            res.render("pages/discover", {events: results.data._embedded.events });
+        })
+        .catch(error => {
+            // Handle errors
+            console.log(error);
+            res.render("pages/discover", { error: true, message: "Failed to get events.", events: [] });
+        });
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.render('pages/logout');
+});
+
+// *****************************************************
+// <!-- Section 5 : Start Server-->
+// *****************************************************
+// starting the server and keeping the connection open to listen for more requests
+app.listen(3000);
+console.log('Server is listening on port 3000');
