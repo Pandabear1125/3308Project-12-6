d55a39674fbd8f691bdc55cd559fe8c1585c7f1d
Emma Kalikstein <emka2886@colorado.edu>
2024-04-07 18:37:50 -0600

    code for timer

--- ProjectSourceCode/src/resources/js/timer.js
+++ ProjectSourceCode/src/resources/js/timer.js
@@ -24,6 +24,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
     //display
     function displayTimer(durationInSeconds) {
-        
+        const timer = document.getElementById("timer");
+        timer.innerHTML = ""; 
+
+        const timerDisplay = document.createElement("div");
+        timer.appendChild(timerDisplay); 
+
+        const interval = setInterval(function() {
+            if (durationInSeconds <= 0) {
+                clearInterval(interval);
+                timerDisplay.textContent = "Time's up!";
+                return; 
+            }
+
+            const minutes = Math.floor(durationInSeconds / 60); 
+            const seconds = durationInSeconds % 60; 
+
+            timerDisplay.textContent = '${minutes}:${seconds < 10 ? O + seconds : seconds}'; 
+            durationInSeconds--; 
+        }, 1000); 
     }
 })
