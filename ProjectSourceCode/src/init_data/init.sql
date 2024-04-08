18285c544c561f6cec2ca095068a227e008d4eda
Pandabear1125 <jvsdestroyer25@gmail.com>
2024-04-08 14:49:05 -0600

    Worked on some querries

--- ProjectSourceCode/src/init_data/init.sql
+++ ProjectSourceCode/src/init_data/init.sql
@@ -12,9 +12,9 @@ CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
-    bio VARCHAR(250),
-    earned_points INT,
-    current_points INT
+    bio VARCHAR(250) DEFAULT '',
+    earned_points INT DEFAULT 0,
+    current_points INT DEFAULT 0
 );
 
 CREATE TABLE games (
@@ -104,18 +104,51 @@ VALUES
 -------------------------------------------------------------
 
 -- Find all moves given a game id
--- SELECT (moves.num, moves.move) FROM moves 
---     JOIN games_to_moves
---     ON moves.move_id = games_to_moves.move_id
--- WHERE games_to_moves.game_id = 1
--- ORDER BY moves.num ASC;
+-- Takes a game id ($1)
+-- Returns array of [[move_number, move_move], ...] ordered by move_number
+
+-- SELECT (moves.num, moves.move) FROM moves JOIN games_to_moves ON moves.move_id = games_to_moves.move_id WHERE games_to_moves.game_id = $1 ORDER BY moves.num ASC;
+
 
 -- Find all of a user's games
--- SELECT games FROM users
--- JOIN users_to_games
--- ON users_to_games.user_id = users.user_id
--- JOIN games
--- ON users_to_games.game_id = games.game_id
--- WHERE users.user_id = 2;
+-- Takes a user id ($1)
+-- Returns array of [[game_id, game_date, game_player_white, game_player_black], ...]
+-- NOTE player may be either white or black, you need to check which one
 
--- Update a user's current points
+-- SELECT (games.game_id, games.date, games.player_white, games.player_black) FROM users JOIN users_to_games ON users_to_games.user_id = users.user_id JOIN games ON users_to_games.game_id = games.game_id WHERE users.user_id = $1;
+
+
+-- Give points to user
+-- Takes a value ($1) and a user ($2)
+-- Updates both earned_points and current_points, should only be used to add points
+
+-- UPDATE users SET earned_points = earned_points + $1, current_points = current_points + $1 WHERE users.user_id = $2;
+
+
+-- Take away points from user
+-- Takes a value ($1) and a user ($2)
+-- Updates only current_points, should only be used to subtract points
+
+-- UPDATE users SET current_points = current_points + $1 WHERE users.user_id = $2;
+
+
+-- Update user's bio
+-- Takes a string value ($1) and a user ($2)
+-- Updates bio only
+
+-- UPDATE users SET bio = $1 WHERE users.user_id = $2;
+
+
+-- Create user
+-- Takes in a username ($1), password ($2), and bio ($3)
+-- Returns the user added in form [user_id, username, password, bio, earned_points, current_points]
+
+-- INSERT INTO users (username, password, bio) VALUES ($1, $2, $3) RETURNING *;
+
+-- IN PROGRESS
+
+-- Create game
+-- INSERT INTO games (date, player_white, player_black) VALUES ('test', 1, 1) RETURNING (game_id);
+
+-- SELECT * FROM games;
+-- SELECT * FROM users_to_games;
